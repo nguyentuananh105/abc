2e95e766e1f86ad8648d73a9deb7e38695edc5e1
Nguyen Tuan Anh <nguyen.tuan.anh@framgia.com>
2013-09-24 16:46:57 +0700

    as

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Abc::Application.config.secret_key_base = '40400d9ae41ef80a838d0affa32edd1551ee16bdee9ce885e83342c9494f954ae16a9d31a43ff22a48e64d38c0498fae692e1e8e0abcb167bc55307b02cd464c'
